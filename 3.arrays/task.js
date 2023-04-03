8992bdb43297af56a7813c88cc875d47d8c7ab4a
Ilya Romanov <rmv17491@gmail.com>
2023-04-03 20:01:15 +0500

    Третье Задание

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,21 @@
 function compareArrays(arr1, arr2) {
-  
-}
+    if (arr1.length !== arr2.length) {
+      return false;
+    }
+    return arr1.every(function(element, index) {
+      return element === arr2[index];
+    });
+  }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
-}
+  function getUsersNamesInAgeRange(users, gender) {
+    const filteredUsers = users.filter(function(user) {
+      return user.gender === gender;
+    });
+    if (filteredUsers.length === 0) {
+      return 0;
+    }
+    const agesSum = filteredUsers.reduce(function(sum, user) {
+      return sum + user.age;
+    }, 0);
+    return agesSum / filteredUsers.length;
+  }
